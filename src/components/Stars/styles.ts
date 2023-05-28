adc251307ed444b15c964448522cd2c3d67d8f19
Gustavo Vieira <gustavovieiradev@gmail.com>
2023-05-28 11:06:15 -0300

    first commit

---
+++ src/components/Stars/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  canvas: {
+    width: 257,
+    height: 249,
+    position: 'absolute',
+    zIndex: 1
+  },
+  container: {
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  }
+})
---
+++ src/screens/History/index.tsx
@@ -0,0 +1,103 @@
+import { useEffect, useRef, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { View, ScrollView, TouchableOpacity, Alert, Pressable } from 'react-native';
+import { HouseLine, Trash } from 'phosphor-react-native';
+
+import { Header } from '../../components/Header';
+import { HistoryCard, HistoryProps } from '../../components/HistoryCard';
+
+import { styles } from './styles';
+import { historyGetAll, historyRemove } from '../../storage/quizHistoryStorage';
+import { Loading } from '../../components/Loading';
+import Animated, { Layout, SlideInRight, SlideOutRight } from 'react-native-reanimated';
+import { Swipeable } from 'react-native-gesture-handler';
+import { THEME } from '../../styles/theme';
+
+export function History() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [history, setHistory] = useState<HistoryProps[]>([]);
+
+  const { goBack } = useNavigation();
+
+  const swipeableRefs = useRef<Swipeable[]>([]);
+
+  async function fetchHistory() {
+    const response = await historyGetAll();
+    setHistory(response);
+    setIsLoading(false);
+  }
+
+  async function remove(id: string) {
+    await historyRemove(id);
+
+    fetchHistory();
+  }
+
+  function handleRemove(id: string, index: number) {
+    swipeableRefs.current?.[index].close()
+    Alert.alert(
+      'Remover',
+      'Deseja remover esse registro?',
+      [
+        {
+          text: 'Sim', onPress: () => remove(id)
+        },
+        { text: 'Não', style: 'cancel' }
+      ]
+    );
+
+  }
+
+  useEffect(() => {
+    fetchHistory();
+  }, []);
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  return (
+    <View style={styles.container}>
+      <Header
+        title="Histórico"
+        subtitle={`Seu histórico de estudos${'\n'}realizados`}
+        icon={HouseLine}
+        onPress={goBack}
+      />
+
+      <ScrollView
+        contentContainerStyle={styles.history}
+        showsVerticalScrollIndicator={false}
+      >
+        {
+          history.map((item, index) => (
+            <Animated.View
+              layout={Layout.springify()}
+              entering={SlideInRight}
+              exiting={SlideOutRight}
+              key={item.id}
+            >
+              <Swipeable 
+                overshootLeft={false}
+                containerStyle={styles.swipeableContainer}
+                // onSwipeableOpen={() => {}}
+                renderLeftActions={() => (
+                  <Pressable style={styles.swipeableRemove} onPress={() => handleRemove(item.id, index)}>
+                    <Trash size={32} color={THEME.COLORS.GREY_100} />
+                  </Pressable>
+                )}
+                ref={(ref) => {
+                  if (ref) {
+                    swipeableRefs.current.push(ref)
+                  }
+                }}
+              >
+                <HistoryCard data={item} />
+              </Swipeable>
+            </Animated.View>
+          ))
+        }
+      </ScrollView>
+    </View>
+  );
+}
